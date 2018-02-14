f1531b6deb9cdd1a62ec22203f2d07256d270e5c
ecky-l <eckhard.lehmann@web.de>
2018-02-14 11:42:49 +0100

    * fixed most owner/uid to variable * added empty diaspora_uploads archive to be extracted to docker volume * fixed build_diaspora script to generate source.tar.gz and copy script/server * own version of script/server without git dependency copied to image

--- roles/diaspora/files/diaspora-build/build_diaspora.sh
+++ roles/diaspora/files/diaspora-build/build_diaspora.sh
@@ -6,14 +6,21 @@ rvm autolibs read-fail
 rvm install 2.4
 
 git clone -b master https://github.com/diaspora/diaspora.git /var/local/diaspora/diaspora
-cd /var/local/diaspora/diaspora
 
+cd /var/local/diaspora/diaspora
 VERSION=$(git tag | tail -n1)
 if [[ -n "${git_tag+set}" ]]; then
 	VERSION=${git_tag}
 fi
 git checkout ${VERSION}
 
+cp -f /var/local/diaspora/script/server /var/local/diaspora/diaspora/script/
+
+# create the tar archive before we create our configs
+tar -cvzf source.tar.gz *
+mv source.tar.gz /var/local/diaspora/diaspora/public/
+
+# copy our changes
 cp -f /var/local/diaspora/config/{diaspora.yml,database.yml,routes.rb} /var/local/diaspora/diaspora/config/
 
 gem install bundler
@@ -28,9 +35,6 @@ RAILS_ENV=production /var/local/diaspora/diaspora/bin/rake db:migrate
 RAILS_ENV=production /var/local/diaspora/diaspora/bin/rake assets:precompile
 
 rm -rf /var/local/diaspora/diaspora/.git
-cd /var/local/diaspora
-tar -cvzf source.tar.gz diaspora
-mv source.tar.gz /var/local/diaspora/diaspora/public/
 tar -cvz --preserve-permissions --same-owner -f diaspora-${VERSION}.tar.gz \
 	.bashrc .bash_profile .bundle .gem .gnupg .mkshrc .pki .profile .rvm diaspora
 rm -rf /var/local/diaspora/{diaspora,.bash_profile,.bashrc,.bundle,.gem,.gnupg,.mkshrc,.pki,.profile,.rvm,.zlogin,.zshrc}
